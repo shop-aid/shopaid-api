8443ce75f932e44dd854188fd1a679f2b3873aba
Dunya Kirkali <dunyakirkali@gmail.com>
2018-06-08 19:52:53 +0200

    Add partners

---
+++ app/views/partners/_partner.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! partner, :id, :name, :percentage, :created_at, :updated_at
+json.url partner_url(partner, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180608165159) do
+ActiveRecord::Schema.define(version: 20180608175241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20180608165159) do
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
     t.string   "category"
+  end
+
+  create_table "partners", force: :cascade do |t|
+    t.string   "name"
+    t.float    "percentage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "taggings", force: :cascade do |t|
