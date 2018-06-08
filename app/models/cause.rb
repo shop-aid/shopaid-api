e26c84f2daebdb7a0cb0d355a697ac9d4909b51a
Dunya Kirkali <dunyakirkali@gmail.com>
2018-06-08 18:55:45 +0200

    Add (fish) tags

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,4 +1,8 @@
 class Cause < ApplicationRecord
+  # Paperclip
   has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
+
+  # Tags
+  acts_as_taggable
 end
--- app/views/api/v1/causes/_cause.json.jbuilder
+++ app/views/api/v1/causes/_cause.json.jbuilder
@@ -3,3 +3,4 @@ json.description cause.description
 json.local cause.local
 json.favorite cause.favorite
 json.logo cause.logo.url(:medium)
+json.category cause.category
